b05f17999ec506836ea337839c27325579d4d551
PC <PC@Admin-PC>
2026-04-13 19:47:04 +0700

    commit

---
+++ Introduction/src/SelIntroduction.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class SelIntroduction {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+		Select dropdown= new Select(staticDropdown);
+		dropdown.selectByIndex(3);
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+	}
+
+}
